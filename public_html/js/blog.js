9c94113aa462418e05505ce6b08c2d033b54e3c7
Peter Lu <peterlu101@yahoo.com>
2016-03-25 08:42:25 -0700

    Completed Video 9 I have added a account to the blog

--- public_html/js/blog.js
+++ public_html/js/blog.js
@@ -1,7 +1,12 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
+$(function() {
+   var APPLICATION_ID = "F8660EB3-8C6F-50F0-FF07-A63709C25C00";
+   var SECERT_KEY = "E5B362B2-FD0C-AED8-FF42-CB0C45AA2E00";
+   var VERSION = "v1";
+   
+   Backhandless.initApp(APPLICATION_ID, SECERT_KEY, VERSION);
+   
+   var user = new Backhandless.user();
+   user.email = "peterlu101@yahoo.com";
+   user.password = "dreamcenter";
+   Backhandless.userService.register(user);
+});
