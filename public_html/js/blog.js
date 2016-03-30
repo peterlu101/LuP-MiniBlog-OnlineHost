f62658aac228de635152d470573a7f993840be39
Peter Lu <peterlu101@yahoo.com>
2016-03-29 21:13:45 -0700

    Completed Video 10 I have finally fixed my error.

--- public_html/js/blog.js
+++ public_html/js/blog.js
@@ -1,12 +1,13 @@
-$(function() {
-   var APPLICATION_ID = "F8660EB3-8C6F-50F0-FF07-A63709C25C00";
-   var SECERT_KEY = "E5B362B2-FD0C-AED8-FF42-CB0C45AA2E00";
-   var VERSION = "v1";
+$(function () {
+    var APPLICATION_ID = "F8660EB3-8C6F-50F0-FF07-A63709C25C00",
+        SECRET_KEY = "E5B362B2-FD0C-AED8-FF42-CB0C45AA2E00",
+        VERSION = "v1";
+        
+    Backendless.initApp(APPLICATION_ID, SECRET_KEY, VERSION);
+    
+    var user = new Backendless.User();
+    user.email = "peterlu101@yahoo.com";
+    user.password = "password";
+    Backendless.UserService.register(user);
    
-   Backhandless.initApp(APPLICATION_ID, SECERT_KEY, VERSION);
-   
-   var user = new Backhandless.user();
-   user.email = "peterlu101@yahoo.com";
-   user.password = "dreamcenter";
-   Backhandless.userService.register(user);
 });
